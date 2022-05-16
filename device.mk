9dc626f93f5067c84e5a775e07c863c0a5e6bc71
LuK1337 <priv.luk@gmail.com>
2022-05-16 13:28:25 +0100

    spaced: Import sensors/2.0/multihal
    Change-Id: I83e469e4e6ecb348369d0ac555f47e040fac4ec5

--- device.mk
+++ device.mk
@@ -354,13 +354,10 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0 \
-    android.hardware.sensors@2.0 \
-    android.hardware.sensors@2.0.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.hardware.sensors@2.1
-
-PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0-service-multihal.spaced \
     libsensorndkbridge
 
 # Soundtrigger
