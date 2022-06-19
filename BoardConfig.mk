3bf56556c7221d86b92892330bca48fcb3f96982
Aayush Gupta <aayushgupta219@gmail.com>
2022-06-19 20:08:41 +0100

    spaced: Inherit sepolicy for mediatek devices
    Change-Id: Id615f35a0d896a6fe8646f9755a06781ec189715
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
