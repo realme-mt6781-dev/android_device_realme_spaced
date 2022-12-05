3eec21dc7848db58768df60f8af5a7032ae04b55
Himanshu Tyagi <himanshut567@gmail.com>
2022-12-05 09:15:22 +0530

    spaced: Label light hal
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: Ic66679b6db017c0f865bc4552fdca6fbfc899033
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,6 +124,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
