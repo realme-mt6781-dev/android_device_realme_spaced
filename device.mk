09ce60db75e16fe9bd612304e750f938b64c81c3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-10 10:25:21 +0000

    spaced: Enforce RRO for all overlays
    Change-Id: Iceb177c9b5322354b8b9f9ce81a91dc04a2350a8
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
