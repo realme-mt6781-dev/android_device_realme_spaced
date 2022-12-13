7595f8b3cb0f952aac3f08c9c03bdc4bd2e44081
LuK1337 <priv.luk@gmail.com>
2022-12-13 19:13:56 +0000

    spaced: Build libhidltransport/libhwbinder/libhardware * Needed for old blobs
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -95,6 +95,15 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhardware \
+    libhardware.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
