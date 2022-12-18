2e41d873bf8e41210d986390eb49869eb52b978c
SamarV-121 <samarvispute121@pm.me>
2022-12-18 20:53:20 +0000

    spaced: Build Wifi services from source
    Change-Id: Ia75aae1533b0f3d1cb759f21137f416fdeca9123

--- device.mk
+++ device.mk
@@ -410,6 +410,9 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service-lazy.spaced
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
