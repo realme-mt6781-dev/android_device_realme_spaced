20e0d8396dcb6ff5deb789d89ea4c6a1ff69ccb7
LuK1337 <priv.luk@gmail.com>
2022-12-19 17:33:27 +0000

    spaced: Add libcamera_metadata shim for stripping out system camera cap
    Change-Id: I8f7772fc9ec15a00c4b630866aa7fc162a52c1aa
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -51,6 +51,9 @@ done
 
 function blob_fixup {
     case "$1" in
+        vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
+            grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
+            ;;
         vendor/lib/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
             ;&
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
