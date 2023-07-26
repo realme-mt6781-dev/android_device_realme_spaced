fd18194722a0d23fc609611f02d00c220332c262
Matsvei Niaverau <matvej2002@gmail.com>
2023-07-26 13:17:16 +0200

    spaced: Build PowerOffAlarm
    Change-Id: I952d4953574ad9db48acb99721f5919110b044c2
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml
 
+# PowerOffAlarm
+PRODUCT_PACKAGES += \
+    PowerOffAlarm
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
