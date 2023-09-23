realme-mt6781-dev/android_device_realme_spaced_old

spaced: Define Bootloader and platform flags
Change-Id: I315245a5d11b343a2273a1225b48a86271d51831
Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

BoardConfig.mk
@@ -21,5 +21,12 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := OPLUS6781
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6781
+
 # Inherit the proprietary files
 include vendor/realme/spaced/BoardConfigVendor.mk

realme-mt6781-dev/android_device_realme_spaced

BoardConfig.mk
@@ -21,5 +21,12 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := oppo6781
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6781
+
 # Inherit the proprietary files
 include vendor/realme/spaced/BoardConfigVendor.mk
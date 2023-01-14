633e8c0f04d5beb9642ad6ae68b30a28e5d15ad1
HELLINFIX <mridulsharma57570@gmail.com>
2023-01-14 17:05:30 +0000

    spaced: Define Bootloader and platform flags
    Change-Id: I315245a5d11b343a2273a1225b48a86271d51831
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,12 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := oppo6781
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6781
+
 # Inherit the proprietary files
 include vendor/realme/spaced/BoardConfigVendor.mk
