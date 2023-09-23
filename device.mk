9c48ea6d63395aaa6d4715b17532cbf67d7504f5
HELLINFIX <mridulsharma57570@gmail.com>
2023-09-23 20:18:17 +0000

    spaced: Build required DRM interfaces
    Change-Id: I04c53ee47d813ab67f23b29c1f98f401eb1dda07
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -61,6 +61,14 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6.vendor \
     libcamera2ndk_vendor
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
