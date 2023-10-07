c2bbbc9a1d2286eb37bf1fa4139b34451a003796
HELLINFIX <mridulsharma57570@gmail.com>
2023-10-07 15:41:46 +0000

    spaced: CipherOS Bringup
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,14 @@
 #
 # Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2023 The CipherOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spaced.mk
+    $(LOCAL_DIR)/cipher_spaced.mk
+
+COMMON_LUNCH_CHOICES := \
+    cipher_spaced-user \
+    cipher_spaced-userdebug \
+    cipher_spaced-eng
