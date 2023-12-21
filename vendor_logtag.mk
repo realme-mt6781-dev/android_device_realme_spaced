e3676bbd6aa1b69c3b74e918bd68b7ab7b4605d1
bengris32 <bengris32@protonmail.ch>
2023-12-21 23:16:20 +0000

    spaced: Silence MDP logspam
    * MDP spams the log an awful amount during video playback and
      is a major contributor to our lagging issue during video playback.
    
    Change-Id: Ib21c73e397f147607de8f1eba22790920cf8de1d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -261,3 +261,4 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.SurfaceControl=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.GraphicBuffer=$(VENDOR_LOG_LEVEL)
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
