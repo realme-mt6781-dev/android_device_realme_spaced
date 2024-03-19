5cb5d12b8c4bf9735793c5e884d7fe9fec0c5a46
Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>
2024-03-19 18:03:18 +0000

    Revert "spaced: Build Oplus Touch HAL and address denials"
    This reverts commit 1676d65e10d1632b69304d0106499e5851f46a44.

--- device.mk
+++ device.mk
@@ -474,12 +474,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@1.0-impl
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch-service.oplus
-
-$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
