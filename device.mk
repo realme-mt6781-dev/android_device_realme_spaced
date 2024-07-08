76d2296eebea0ca2f004d46ded8329bebbb4601d
inferno0230 <mail@inferno0230.in>
2024-07-08 08:32:05 +0530

    [TEMP] spaced:  Copy prebuilt on_vendor apex from our last working lineage build
    Change-Id: I9e4e05d726ca74050100721e4f307ed8d9ac2bf2

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Apex
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/com.android.vndk.current.on_vendor.apex:$(TARGET_COPY_OUT_VENDOR)/apex/com.android.vndk.current.on_vendor.apex
+
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
