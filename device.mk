6474f02163e4ab412741e2de788e6f964ce6ce16
HELLINFIX <mridulsharma57570@gmail.com>
2025-07-21 07:03:20 +0000

    Revert "spaced: Move to common QTI Vibrator service"
    This reverts commit 4896eac06b0016da072ac44da6ed43d6af77b402.

--- device.mk
+++ device.mk
@@ -485,13 +485,10 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+$(call soong_config_set,mediatek_vibrator,supports_effects,true)
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    android.hardware.vibrator-service.mediatek
 
 # VNDK
 PRODUCT_PACKAGES += \
