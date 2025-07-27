45ec22ec29fdbeab97236e7e2b5d779bf12a3321
HELLINFIX <mridulsharma57570@gmail.com>
2025-07-27 11:40:18 +0000

    spaced: Welcome AxionOS

--- lineage_spaced.mk
+++ lineage_spaced.mk
@@ -14,6 +14,22 @@ $(call inherit-product, device/realme/spaced/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Axion Stuff
+PRODUCT_NO_CAMERA := false
+AXION_CAMERA_REAR_INFO := 50,2,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_MAINTAINER := HELLINFIX
+AXION_PROCESSOR := MTK_Helio_G96
+
+# Axion Optional Flags
+TARGET_ENABLE_BLUR := true
+PERF_ANIM_OVERRIDE := true
+TARGET_USES_SLMK := true
+TARGET_INCLUDES_LOS_PREBUILTS := false
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := lineage_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_MANUFACTURER := realme
