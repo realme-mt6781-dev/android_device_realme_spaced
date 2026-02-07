2a6770e2e98f126e15ddaddfd2c87eb58c7fc220
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2026-02-07 11:19:55 +0000

    d

--- extract-files.py
+++ extract-files.py
@@ -1,16 +1,28 @@
-#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
-# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
-from extract_utils.fixups_blob import (
-    blob_fixup,
-    blob_fixups_user_type,
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
 )
 
 # -----------------------------------------------------------------------------
@@ -188,8 +200,9 @@ blob_fixups: blob_fixups_user_type = {
 module = ExtractUtilsModule(
     'spaced',
     'realme',
-    namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 # -----------------------------------------------------------------------------
